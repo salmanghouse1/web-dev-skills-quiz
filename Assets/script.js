18d2e7617bfed1f7938534e9953d88bd83390205
Salman Ghouse <ghousemu@sheridancollege.ca>
2021-11-11 14:09:02 -0500

    wrote majority of pseudo code comments

---
+++ Assets/script.js
@@ -0,0 +1,16 @@
+/*psuedo comments
+
+1.add event listener to button
+2. assign variable to array
+3.create 5 arrays with 4 strings in each array, each string is a guess option for each question there are 5 questions
+4.Create condition if start button is clicked
+5create orderd list using js
+6.create div inside is a button using js add a class to it and appendChild a button to each list item use foreach
+7.create div using js as the question use text content to add variable question1 ,question2, question3...so on
+8.appendChild to div container under the question
+9.if ordered list is clicked get the event.target, and get the data attr.
+10.if data-atr is equal to the answer string, add 10 points and display "Correct" text content in a div that is created using js and give it a class
+11.if data-atr is wrong or not equal to the answer string, deduct some time
+12.if time === 0, it is game over text content then restart.
+13.after each answer store a counter var and increment it, if it equals 5 then show the high scores function and enter name, then if name is entered show the highscores using function call and pass the arguments in
+14.clear scores will set the text content of the ol li to blank
